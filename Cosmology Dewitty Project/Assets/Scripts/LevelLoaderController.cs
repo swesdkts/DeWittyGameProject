d1320a0c396b5e4a209e271403ac3e1e1845f6d4
ScottThompson <swesdssdd@gmail.com>
2022-12-15 22:15:42 -0600

    Updated scripts

--- Cosmology Dewitty Project/Assets/Scripts/LevelLoaderController.cs
+++ Cosmology Dewitty Project/Assets/Scripts/LevelLoaderController.cs
@@ -15,15 +15,9 @@ public class LevelLoaderController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            StartCoroutine(LoadScene("SampleScene"));
-        }
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
+        RestartScene();
+        ExitApplication();
+        
     }
 
     /* An IEnumerator for loading scenes with the crossfade animation. 
@@ -36,4 +30,21 @@ public class LevelLoaderController : MonoBehaviour
         yield return new WaitForSeconds(1);
         SceneManager.LoadScene(nextScene.ToString());
     }
+
+    private void RestartScene()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(LoadScene("SampleScene"));
+        }
+    }
+
+    private void ExitApplication()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
 }
