e40ed816d05d506191299210fb39674a9ae54031
ScottThompson <swesdssdd@gmail.com>
2023-04-05 17:12:29 -0500

    Update title screen
    Created a simple title screen to use for presenting prototype.

--- Cosmology Dewitty Project/Assets/Artifact Retrieval Game/SwitchScene.cs
+++ Cosmology Dewitty Project/Assets/Artifact Retrieval Game/SwitchScene.cs
@@ -34,9 +34,24 @@ public class SwitchScene : MonoBehaviour
     // I had to create a coroutine so there would be a delay. The reason C# works this way is because it just does.
     public IEnumerator TeleportCoroutine()
     {
-        teleportParticles.Play();
+        if(teleportParticles != null)
+        {
+            teleportParticles.Play();
+        }
         yield return new WaitForSeconds(1.5f);
+
         crossfade.LoadScene(sceneBuildIndexToLoad);
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(TeleportCoroutine());
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
 }
